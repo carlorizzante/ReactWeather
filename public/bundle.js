579283f573483eca0b796567ba79a6504a3e3d43
Carlo Rizzante <hello@carlorizzante.com>
2016-06-14 14:52:39 +0200

    Fixes server.js for localhost

--- public/bundle.js
+++ public/bundle.js
@@ -1568,7 +1568,7 @@
 /* 254 */
 /***/ function(module, exports, __webpack_require__) {
 
-	eval("\"use strict\";\n\nvar React = __webpack_require__(1);\n\n// var About = React.createClass({\n//   render: function () {\n//     return (\n//       <h3>About component</h3>\n//     );\n//   }\n// });\n\nvar About = function About(props) {\n  return React.createElement(\n    \"h3\",\n    null,\n    \"About!\"\n  );\n};\n\nmodule.exports = About;\n\n/*****************\n ** WEBPACK FOOTER\n ** ./app/components/About.jsx\n ** module id = 254\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./app/components/About.jsx?");
+	eval("\"use strict\";\n\nvar React = __webpack_require__(1);\n\n// var About = React.createClass({\n//   render: function () {\n//     return (\n//       <h3>About component</h3>\n//     );\n//   }\n// });\n\nvar About = function About(props) {\n  return React.createElement(\n    \"div\",\n    null,\n    React.createElement(\n      \"h3\",\n      null,\n      \"About!\"\n    ),\n    React.createElement(\n      \"p\",\n      null,\n      \"Welcome!\"\n    )\n  );\n};\n\nmodule.exports = About;\n\n/*****************\n ** WEBPACK FOOTER\n ** ./app/components/About.jsx\n ** module id = 254\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./app/components/About.jsx?");
 
 /***/ },
 /* 255 */
--- server.js
+++ server.js
@@ -3,17 +3,13 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-if (process.env.PORT) {
-  app.use(function (req, res, next) {
-    if (req.headers["x-forwarded-proto"] === "http") {
-      console.log("All good.");
-      next();
-    } else {
-      console.log("Redirecting...");
-      res.redirect("http://" + req.hostname + req.url);
-    }
-  });
-}
+app.use(function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "https") {
+    res.redirect("http://" + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static("public"));
 
