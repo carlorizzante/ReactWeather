3bf0486d6283fc943a19b7d419830cab63f6c954
Carlo Rizzante <hello@carlorizzante.com>
2016-06-14 14:17:33 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var express = require("express");
+
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "http") {
+    console.log("All good.");
+    next();
+  } else {
+    console.log("Redirecting...");
+    res.redirect("http://" + req.hostname + req.url);
+  }
+});
 
 app.use(express.static("public"));
-app.listen(3000, function () {
-  console.log("Express server running on port 3000.");
+
+app.listen(PORT, function () {
+  console.log("Express server running on port " + PORT + "...");
 });
