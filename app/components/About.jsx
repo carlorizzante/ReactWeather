76deb83a0113a0a05614c48e12e34b2669d35a26
Carlo Rizzante <hello@carlorizzante.com>
2016-06-15 13:27:55 +0200

    Adds content to the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,6 +13,14 @@ var About = (props) => {
     <div>
       <h1>About!</h1>
       <p>Welcome!</p>
+      <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur.</p>
+      <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Sed posuere consectetur est at lobortis. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+      <p>Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a></li>
+        <li><a href="https://openweathermap.org">Open Weather Map</a></li>
+      </ul>
     </div>
   );
 }
